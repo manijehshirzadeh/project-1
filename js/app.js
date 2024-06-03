14427d8d9129ac43cdc78bc20a1ff6c1617a5790
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-06-03 16:12:12 +1000

    draw the board

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+const container = document.querySelector(".container");
 
+for (let i = 0; i < 100; i++) {
+  let element = document.createElement("div");
+  element.className = "cell";
+  element.id = "cell" + i;
+  container.appendChild(element);
+}
+
+let snake = [0];
+
+const render = () => {
+  const element = (document.querySelector("#cell1").style.backgroundColor =
+    "red");
+};
+
+const initialize = () => {
+  render();
+};
+
+initialize();
